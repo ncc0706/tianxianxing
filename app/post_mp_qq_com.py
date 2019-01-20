c0610715ed3051dd3171291191d318514f6c1c93
niuyuxian <ncc0706@gmail.com>
2019-01-20 20:43:47 +0800

    download video

--- app/post_mp_qq_com.py
+++ app/post_mp_qq_com.py
@@ -1,26 +1,37 @@
-from flask import Blueprint, render_template
+import os
+import shutil
 
-mp_qq = Blueprint('post.mp.qq.com', __name__)
+import requests
+from bs4 import BeautifulSoup
+from flask import Blueprint, render_template, request, Request
 
-
-# _base_dir = 'post.mp.qq.com'
-#
-# url = 'http://post.mp.qq.com/kan/nvideo/2500523864-5975c30d62b272aw-shg_1083162092_1047_26879eb73d9546c1a957bf63dd4cvide.html?_wv=2281701505&sig=0088d007af4c3ea6e2d0ab6251fd506f&time=1546704590&web_ch_id=105'
-#
-# r = requests.get(url)
-#
-# soup = BeautifulSoup(r.text, 'html.parser')
-#
-# video_src = soup.find(id='video-play').get('data-video-src')
-# title = soup.find(id='mod-common').get('data-article-title')
-# video = requests.get(video_src, stream=True)
-#
-# if not os.path.exists(_base_dir):
-#     os.mkdir(_base_dir)
-# file = open('{}/{}.mp4'.format(_base_dir, title), 'wb')
-# shutil.copyfileobj(video.raw, file)
+__base_dir = 'post.mp.qq.com'
+mp_qq = Blueprint(__base_dir, __name__)
 
 
 @mp_qq.route('/dl')
 def dl():
     return render_template('mp_qq.html')
+
+
+@mp_qq.route('/dl_to_disk')
+def download_to_disk():
+    url = request.query_string
+    if url is None:
+        return "参数为空"
+
+    url = bytes.decode(url)
+    url = url[url.index('=') + 1:]
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    video_src = soup.find(id='video-play').get('data-video-src')
+    title = soup.find(id='mod-common').get('data-article-title')
+    video = requests.get(video_src, stream=True)
+
+    if not os.path.exists(__base_dir):
+        os.mkdir(__base_dir)
+    file = open('{}/{}.mp4'.format(__base_dir, title), 'wb')
+    shutil.copyfileobj(video.raw, file)
+    return 'download ok'
